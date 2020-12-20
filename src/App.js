41df30da02a6b71f247408b443a13cd42c204426
Armin Ebner <armin_ebner@web.de>
2020-12-20 17:55:01 +0100

    intro and setup page setup without transitioning effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Landing from './components/pages/Landing'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/' component={Landing} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/pages/Landing.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import '../css/landing.css'
+import OpenSkyLogo from '../../img/logos/opensky.png'
+import PlanespottersLogo from '../../img/logos/planespotters.png'
+
+const Landing = () => {
+
+    const [ intro, setIntro ] = useState([])
+
+    const introTimer = () => {
+        setTimeout(() => {
+            setIntro(false)
+        }, 5000);
+        
+    }
+    introTimer()
+
+    return (
+        <>
+            {intro &&
+                <div className='intro'>
+                    <div className="logo-box">
+                        <h1>Aviate</h1>
+                        <h2>...live</h2>
+                    </div>
+                    <div className="landing-footer">
+                        <h3>powered by</h3>
+                        <div className="logo-container">
+                            <img src={OpenSkyLogo} alt="logo from open sky network"/>
+                            <img src={PlanespottersLogo} alt="logo from planespotters.net"/>
+                        </div>
+                    </div>
+                </div>
+            }
+            <div className="setup">
+                <div className="setup-box">
+                        <h2>welcome</h2>
+                        <p className='upper-p'>set your airport (icao)</p>
+                        <input type="text" value="" placeholder="e.g. EDDK"/>
+                        <p className='lower-p'>just show me planes, pls!</p>
+                    </div>
+                    <div className="setup-footer">
+                        <h3>powered by</h3>
+                        <div className="logo-container">
+                            <img src={OpenSkyLogo} alt="logo from open sky network"/>
+                            <img src={PlanespottersLogo} alt="logo from planespotters.net"/>
+                        </div>
+                    </div>
+            </div>
+        </>
+    )
+}
+
+export default Landing

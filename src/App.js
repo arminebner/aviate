beeee83c51fad4a6a21514d7db27c4f5ef5d097b
Armin Ebner <armin_ebner@web.de>
2020-12-28 17:45:38 +0100

    animations with framer motion on single-page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import { DataProvider } from './global/DataContext'
 import Landing from './components/pages/Landing'
-import Login from './components/pages/Login'
 import MapPage from './components/pages/MapPage'
 import './App.css';
 
 function App() {
 
-  let location = useLocation()
-
   return (
     <DataProvider>
       <div className="App">
           <AnimatePresence exitBeforeEnter>
-            <Switch location={ location } key={ location.pathname }>
+            <Switch>
               <Route exact path='/' component={Landing} />
-              <Route path='/login' component={Login} />
               <Route path='/map' component={MapPage} />
             </Switch>
           </AnimatePresence>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import '../css/landing.css'
@@ -8,23 +8,55 @@ import PlanespottersLogo from '../../img/logos/planespotters.png'
 
 const Landing = () => {
 
+    const [ page, setPage ] = useState({ intro: true, setup: false})
+
+    useEffect(() => {
+        setTimeout(() => {
+            setPage({ intro: false, setup: true})
+        }, 5000);
+    }, [])
+
     return (
-        <motion.div exit={{ opacity: 0.1 }} animate={{ opacity: 1 }} initial={{ opacity: 0.1 }}>
-            <div className='intro'>
-                <div className="logo-box">
-                    <h1>Aviate</h1>
-                    <h2>...live</h2>
-                    <NavLink to='/login'><p>start</p></NavLink>
-                </div>
-                <div className="landing-footer">
-                    <h3>powered by</h3>
-                    <div className="logo-container">
-                        <img src={OpenSkyLogo} alt="logo from open sky network"/>
-                        <img src={PlanespottersLogo} alt="logo from planespotters.net"/>
+        <>
+            { page.intro &&
+                <motion.div exit={{ opacity: 0.1 }} animate={{ opacity: 1 }} initial={{ opacity: 0.1 }}>
+                    <div className='intro'>
+                        <div className="logo-box">
+                            <h1>Aviate</h1>
+                            <h2>...live</h2>
+                            <NavLink to='/login'><p>start</p></NavLink>
+                        </div>
+                        <div className="landing-footer">
+                            <h3>powered by</h3>
+                            <div className="logo-container">
+                                <img src={OpenSkyLogo} alt="logo from open sky network"/>
+                                <img src={PlanespottersLogo} alt="logo from planespotters.net"/>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </motion.div>
+                </motion.div> 
+            }
+
+            { page.setup &&
+                <motion.div exit={{ opacity: 0.1 }} animate={{ opacity: 1 }} initial={{ opacity: 0.1 }}>
+                    <div className="setup">
+                        <div className="setup-box">
+                            <h2>welcome</h2>
+                            <p className='upper-p'>set your airport (icao)</p>
+                            <input type="text" value="" placeholder="e.g. EDDK"/>
+                            <NavLink to='/map'><p className='lower-p'>just show me planes, pls!</p></NavLink>
+                        </div>
+                        <div className="setup-footer">
+                            <h3>powered by</h3>
+                            <div className="logo-container">
+                                <img src={OpenSkyLogo} alt="logo from open sky network"/>
+                                <img src={PlanespottersLogo} alt="logo from planespotters.net"/>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+            }
+       </>
     )
 }
 
